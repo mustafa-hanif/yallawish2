a78a2792ae8868836cf773381c6728f70d480e19
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-06 17:49:44 +0500

    chore: labels file

---
+++ lng/english.ts
@@ -0,0 +1,8 @@
+export const labels = {
+  giftingMadePersonal: "Gifting made\npersonal.",
+  splashText: "Build lists for yourself or someone you love and make every gift count.",
+  continueWithGoogle: "Continue with Google",
+  continueWithApple: "Continue with Apple",
+  continueWithEmail: "Continue with Email",
+  or: "OR",
+};
