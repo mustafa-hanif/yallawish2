f783e3ef1242a45a57ddb8bb6e8cd0e569543fc3
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-06 17:51:05 +0500

    feat: new assets are added

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,4 +1,7 @@
 export const Images = {
   splash: require("./splashbackground.png"),
   logo: require("./yallawish_logo.png"),
+  google: require("./google.png"),
+  apple: require("./apple.png"),
+  mail: require("./mail.png"),
 };
