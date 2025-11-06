4aff5927b85a6c8b559c405d889e253c3aaf582d
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-06 17:49:27 +0500

    fix: width set according to the screen dimension

--- app/(auth)/splash/style.ts
+++ app/(auth)/splash/style.ts
@@ -1,9 +1,12 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+const { width } = Dimensions.get("window");
 
 export const style = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    width,
+    resizeMode: "cover",
   },
 });
