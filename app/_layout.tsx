3f141274196c629f23c2f3ef52c9ca65b3b5bcf3
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-21 18:25:06 +0500

    fix: default screen issue fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,7 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -17,15 +13,17 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 import useGoogleFonts from "@/hooks/useFonts";
+import { Dimensions, Platform } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-const convex = new ConvexReactClient(
-  process.env.EXPO_PUBLIC_CONVEX_URL as string
-);
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL as string);
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const { width: SCREEN_WIDTH } = Dimensions.get("window");
+  const isDesktop = Platform.OS === "web" && SCREEN_WIDTH >= 768;
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -42,25 +40,33 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider
-      publishableKey="pk_test_cHJvZm91bmQtc3RhZy04Ny5jbGVyay5hY2NvdW50cy5kZXYk"
-      tokenCache={tokenCache}
-    >
+    <ClerkProvider publishableKey="pk_test_cHJvZm91bmQtc3RhZy04Ny5jbGVyay5hY2NvdW50cy5kZXYk" tokenCache={tokenCache}>
       <ConvexProvider client={convex}>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="desktopHomepage" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="(auth)"
-              options={{ headerShown: false, title: "" }}
-            />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          {isDesktop ? (
+            <>
+              <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="desktopHomepage" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false, title: "" }} />
+                <Stack.Screen name="+not-found" />
+              </Stack>
+              <StatusBar style="auto" />
+            </>
+          ) : (
+            <>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false, title: "" }} />
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="desktopHomepage" options={{ headerShown: false }} />
+
+                <Stack.Screen name="+not-found" />
+              </Stack>
+              <StatusBar style="auto" />
+            </>
+          )}
         </ThemeProvider>
       </ConvexProvider>
     </ClerkProvider>
