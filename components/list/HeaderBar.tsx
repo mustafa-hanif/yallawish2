60512f5b0830ca49ee5611b71ea918a9b024013f
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-21 20:11:28 +0500

    fix: header issue fixed

--- components/list/HeaderBar.tsx
+++ components/list/HeaderBar.tsx
@@ -1,8 +1,7 @@
 import { styles } from "@/styles/addGiftStyles";
-import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type Props = {
@@ -12,17 +11,22 @@ type Props = {
 
 export const HeaderBar: React.FC<Props> = ({ title, onBack }) => {
   return (
-    <LinearGradient colors={["#330065", "#6600CB"]} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} style={styles.header}>
-      <SafeAreaView edges={["top"]}>
-        <View style={styles.headerContent}>
-          <View style={styles.navigation}>
-            {onBack ? <Pressable onPress={onBack} style={styles.backButton}>
-              <Ionicons name="chevron-back" size={24} color="#FFFFFF" />
-            </Pressable> : null}
-            <Text style={styles.headerTitle}>{title}</Text>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" />
+      <LinearGradient colors={["#330065", "#45018ad7"]} style={styles.header} locations={[0, 0.7]} start={{ x: 0, y: 0 }} end={{ x: 0, y: 2 }} style={styles.header}>
+        <SafeAreaView edges={["top"]}>
+          <View style={styles.headerContent}>
+            <View style={styles.navigation}>
+              {onBack ? (
+                <Pressable onPress={onBack} style={styles.backButton}>
+                  <Image source={require("@/assets/images/backArrow.png")} />
+                </Pressable>
+              ) : null}
+              <Text style={styles.headerTitle}>{title}</Text>
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </LinearGradient>
+        </SafeAreaView>
+      </LinearGradient>
+    </>
   );
 };
