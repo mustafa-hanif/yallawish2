5ddd026db6010b1f8f5c33014c7e5cea4ec6a9b5
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-28 18:14:00 +0500

    fix: Link added

--- components/homepage/Mobile/UpcomingEvents/EventCard/index.tsx
+++ components/homepage/Mobile/UpcomingEvents/EventCard/index.tsx
@@ -1,4 +1,5 @@
 import { occasionObj } from "@/constants/Occasion";
+import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "./style";
@@ -12,23 +13,25 @@ export default function EventCard({ eventDetails }: EventCardProps) {
   const occasion = occasionObj[String(eventDetails?.occasion) || "birthday"];
   const count = 8;
   return (
-    <View style={[styles.card, { borderColor: occasion?.borderColor }]}>
-      <View>
-        <Text style={styles.date}>{eventDetails.date}</Text>
-        <Text style={styles.month}>{eventDetails.month}</Text>
-      </View>
-      <View style={styles.titleAndGiftsPurchasedContainer}>
+    <Link href={{ pathname: "/add-gift", params: { listId: String(eventDetails.id) } }}>
+      <View style={[styles.card, { borderColor: occasion?.borderColor }]}>
         <View>
-          <Text style={styles.eventTitle} numberOfLines={2}>
-            {eventDetails?.title}
-          </Text>
+          <Text style={styles.date}>{eventDetails.date}</Text>
+          <Text style={styles.month}>{eventDetails.month}</Text>
         </View>
-        <View>
-          <View style={[styles.giftPurchasedPill, { backgroundColor: occasion?.backgroundColor }]}>
-            <Text style={[styles.giftPurchasedText, { color: occasion?.borderColor }]}>Gifts purchased: {count}</Text>
+        <View style={styles.titleAndGiftsPurchasedContainer}>
+          <View>
+            <Text style={styles.eventTitle} numberOfLines={2}>
+              {eventDetails?.title}
+            </Text>
+          </View>
+          <View>
+            <View style={[styles.giftPurchasedPill, { backgroundColor: occasion?.backgroundColor }]}>
+              <Text style={[styles.giftPurchasedText, { color: occasion?.borderColor }]}>Gifts purchased: {count}</Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </Link>
   );
 }
