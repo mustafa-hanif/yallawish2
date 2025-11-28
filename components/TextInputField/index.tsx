9636f1992e6a8f29601e476a5a52e1c6ab69f564
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-11-28 12:03:03 +0500

    fix: extra space below the input field fixed

--- components/TextInputField/index.tsx
+++ components/TextInputField/index.tsx
@@ -17,9 +17,8 @@ type TextInputFieldProps = {
   keyboardType?: TextInputProps["keyboardType"];
   autoCapitalize?: TextInputProps["autoCapitalize"];
   autoCorrect?: boolean;
-  error?: string[] | string;
-  inputLabelContainerStyle?: ViewStyle
-
+  error?: string[] | string | null;
+  inputLabelContainerStyle?: ViewStyle;
 };
 
 export function TextInputField({ icon, label, autoCorrect, autoCapitalize, variant = "default", keyboardType, placeholder, value, onChangeText, placeholderTextColor = "#D1D1D6", error, inputLabelContainerStyle }: TextInputFieldProps) {
@@ -28,14 +27,13 @@ export function TextInputField({ icon, label, autoCorrect, autoCapitalize, varia
   return (
     <View style={styles.container}>
       <View style={styles.inputWrapper}>
-        <View style={{...styles.labelContainer, ...inputLabelContainerStyle}}>
+        <View style={{ ...styles.labelContainer, ...inputLabelContainerStyle }}>
           <Text style={styles.label}>{label}</Text>
         </View>
         <TextInput autoCorrect={autoCorrect} keyboardType={keyboardType} autoCapitalize={autoCapitalize} style={styles.input} value={value} onChangeText={onChangeText} placeholder={placeholder} placeholderTextColor={placeholderTextColor} />
         {icon && <View style={styles.rightIconContainer}>{icon}</View>}
-
       </View>
-      {Array.isArray(error) ? <>{error?.map((errorItem) => <Text style={styles.errorText}>{errorItem || ""}</Text>)}</> : <Text style={styles.errorText}>{error || ""}</Text>}
+      {error ? <>{Array.isArray(error) ? <>{error?.map((errorItem) => <Text style={styles.errorText}>{errorItem || ""}</Text>)}</> : <Text style={styles.errorText}>{error || ""}</Text>}</> : <></>}
     </View>
   );
 }
