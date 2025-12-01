2aa2dca7ab9a4d4284947c2dd9dff5cf09e8e724
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-12-01 14:36:28 +0500

    fix: initial screen rendering issue fixed

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,33 @@
+import { useAuth } from "@clerk/clerk-expo";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+
 import { Desktop } from "@/components/homepage/Desktop";
 import { Mobile } from "@/components/homepage/Mobile";
-import React from "react";
-
-import { Dimensions, Platform } from "react-native";
+import { router } from "expo-router";
+import { Dimensions, Platform, View } from "react-native";
 
 const DESKTOP_BREAKPOINT = 1024;
 
 export default function HomeScreen() {
   const { width: SCREEN_WIDTH } = Dimensions.get("window");
   const isDesktop = Platform.OS === "web" && SCREEN_WIDTH >= DESKTOP_BREAKPOINT;
+  const { isSignedIn } = useAuth();
+  const [currentDevice, setCurrentDevice] = useState<string | null>(null);
 
-  if (isDesktop) {
-    return <Desktop />;
-  }
-  return <Mobile />;
+  useLayoutEffect(() => {
+    if (isDesktop) {
+      setCurrentDevice("desktop");
+    } else {
+      setCurrentDevice("mobile");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isSignedIn && !isDesktop) router.replace("/(tabs)");
+  }, [router, isDesktop]);
+
+  if (currentDevice) {
+    if (currentDevice === "desktop") return <Desktop />;
+    else return <Mobile />;
+  } else return <View />;
 }
