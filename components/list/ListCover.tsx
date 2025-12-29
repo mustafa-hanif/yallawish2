95d4564c61701817442cf609bf499c991b693593
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2025-12-29 15:27:32 +0500

    chore: Add creator profile to ListCover component
    ListCover now accepts a 'creator' prop and displays the creator's profile image or initials in the cover overlay. Utilizes getProfileInitials utility for fallback initials display.

--- components/list/ListCover.tsx
+++ components/list/ListCover.tsx
@@ -1,23 +1,24 @@
 import { styles } from "@/styles/addGiftStyles";
+import { getProfileInitials } from "@/utils";
 import React from "react";
 import { Image, Text, View } from "react-native";
 
 type Props = {
   imageUri?: string | null;
   overlayText?: string;
-  occasion?: string
+  occasion?: string;
+  creator?: { firstName?: string; lastName?: string; profileImageUrl?: string; email?: string } | null;
 };
 
-export const ListCover: React.FC<Props> = ({ imageUri, overlayText, occasion }) => {
-
+export const ListCover: React.FC<Props> = ({ imageUri, overlayText, occasion, creator = null }) => {
+  const profileInitials = getProfileInitials(creator, "YW");
   return (
     <View style={styles.coverContainer}>
-      {imageUri ? (
-        <Image source={{ uri: imageUri }} style={styles.coverImage} />
-      ) : (
-        <Image source={require("@/assets/images/nursery.png")} style={styles.coverImage} />
-      )}
-      <View style={styles.coverOverlay}>{!!overlayText && <Text style={styles.daysToGo}>{overlayText}</Text>}</View>
+      {imageUri ? <Image source={{ uri: imageUri }} style={styles.coverImage} /> : <Image source={require("@/assets/images/nursery.png")} style={styles.coverImage} />}
+      <View style={styles.coverOverlay}>
+        {creator && <View style={styles.listCoverProfile}>{creator?.profileImageUrl ? <Image resizeMode="cover" style={styles.listCoverProfileImageUrl} source={{ uri: creator.profileImageUrl }} /> : <Text style={styles.listCoverProfileInitials}>{profileInitials}</Text>}</View>}
+        {!!overlayText && <Text style={styles.daysToGo}>{overlayText}</Text>}
+      </View>
     </View>
   );
 };
