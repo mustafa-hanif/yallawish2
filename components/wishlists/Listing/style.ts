81a78197b903c17e2e9eead25e6aab5b1b6fffc3
MuhammadMujtabaa <51963629+MuhammadMujtabaa@users.noreply.github.com>
2026-01-20 11:55:59 +0500

    Update Listing container styles for centering
    Added width: '100%', justifyContent: 'center', and alignItems: 'center' to the container style in Listing to ensure content is centered and spans full width.

--- components/wishlists/Listing/style.ts
+++ components/wishlists/Listing/style.ts
@@ -3,7 +3,10 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     paddingVertical: 8,
+    justifyContent:'center',
+    alignItems:'center'
   },
   contentContainerStyle: {
     gap: 8,
